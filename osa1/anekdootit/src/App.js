69a2a2968da1c7efa6e96f7aaaad81d91625f243
Maijjay <maija.ylitalo95@gmail.com>
2022-01-24 18:39:30 +0200

    updating

---
+++ osa1/anekdootit/src/App.js
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>  
+)
+
+const Anecdote = (props) => {
+  const s = props.selected
+  return (
+    <>
+      <p>{props.anecdotes[s]} </p> 
+    </>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <h1>
+      {props.text}
+    </h1> 
+  )
+}
+
+const BestAnecdote = (props) => {
+  return (
+    <>
+      1
+    </>
+  )
+}
+  
+
+const App = (props) => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+  const [votes, setVotes]= useState({
+    votes: new Uint8Array(anecdotes.length)
+  })
+
+  const [selected, setSelected] = useState({
+    selected: 0
+  })
+
+  const handleVoteClick = (props) => {
+    const copy = [{...votes}]
+    copy[1] += 1
+    console.log('vote', copy)
+
+    setVotes({...copy, votes: copy })
+    console.log(selected)
+    console.log('vote', copy)
+
+  }
+  
+  const handleNextClick = () => {
+    setSelected({...selected, selected: (Math.floor(Math.random() * anecdotes.length))})
+    console.log(selected)
+  }
+
+  return (
+    <div>
+      <Header text = 'Anecdote of the day'/>
+      <Button handleClick = {handleVoteClick} text = 'vote' Button/>
+      <Button handleClick = {handleNextClick} text = 'next anecdote' Button/>
+      <Anecdote anecdotes={anecdotes} selected={selected.selected}/>
+      <Header text = 'Anecdote with most votes'/>
+      <BestAnecdote />
+    </div>
+  )
+}
+
+export default App
--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,50 @@
 import { useState } from 'react'
 
+
+const Header = ()=> {
+  return (
+    <h1>give feedback</h1>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+   <div>
+     <>
+      {props.text} {props.value}
+     </>
+   </div>
+  )
+}
+
+const Statistics = (props) => {
+  if(props.all == 0){
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+  const average = (props.good + (-1 * props.bad)) / props.all
+  const positive = (props.good/props.all) * 100  
+  return (
+    <>
+      <h1>statistics</h1>
+      <tr>
+        <StatisticLine text="good:" value={props.good}/>
+        <StatisticLine text="neutral:" value={props.neutral}/>
+        <StatisticLine text="bad:" value={props.bad}/>
+        <StatisticLine text="all:" value={props.all}/>
+        <StatisticLine text="average:" value={average}/>
+        <StatisticLine text="positive:" value={positive} />
+      </tr>
+    </>
+  )
+}
+
+
+
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
@@ -11,15 +56,7 @@ const App = (props) => {
     good: 0, neutral: 0, bad: 0, all: 0
   })
 
-  const Header = ()=> {
-    return (
-      <h1>give feedback</h1>
-    )
-  }
-
   const handleGoodClick = () => {
-    console.log(props)
-
     setClicks({...clicks, good: clicks.good + 1, all: clicks.all +1})
   }
 
@@ -31,28 +68,18 @@ const App = (props) => {
     setClicks({...clicks, bad: clicks.bad + 1, all: clicks.all +1})
   }
 
-  const Statistics = (props) => {
-    console.log(props)
-
+  
     return (
-      <>
-      <h1>statistics</h1>
-      <p>good: {props.good}</p>
-      </>
-      )
-  }
-    return (
-    <div>
-      <>
-        <Header/>
-        <Button handleClick = {handleGoodClick} text='good' Button/>
-        <Button handleClick = {handleNeutralClick} text ='neutral' Button/>
-        <Button handleClick = {handleBadClick} text='bad' Button/>
-        <Statistics good={props.good}/>
-        
-      </>
-    </div>
-  );
+      <div>
+        <>
+          <Header/>
+          <Button handleClick = {handleGoodClick} text='good' Button/>
+          <Button handleClick = {handleNeutralClick} text ='neutral' Button/>
+          <Button handleClick = {handleBadClick} text='bad' Button/>
+          <Statistics good={clicks.good} neutral={clicks.neutral} bad={clicks.bad} all={clicks.all}/>
+        </>
+      </div>
+    );
 }
 
 export default App;
